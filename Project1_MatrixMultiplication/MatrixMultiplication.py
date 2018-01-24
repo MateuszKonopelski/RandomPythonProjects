e4f2d3de8788d2822407f0a0d7aba78dfd027425
Matt <panpluto@gmail.com>
2018-01-24 23:23:19 +0100

    Update MatrixMultiplication.py

--- Project1_MatrixMultiplication/MatrixMultiplication.py
+++ Project1_MatrixMultiplication/MatrixMultiplication.py
@@ -2,24 +2,22 @@
 # The matrix elements are filled by User
 # The result is set up and saved as list
 
-print('''
-MATRIX MULTIPLICATION\n
-''')
+print('MATRIX MULTIPLICATION\n')
 
-# Ask user for
+# Ask user for dimensions of first matrix
 r = int(input("Matrix1: Number of rows?: "))
 c = int(input("Matrix1: Number of columns?: "))
 
-# Print design matrix with user dimensions to easier visualize M11, M13 etc
+# Print design of first Matrix for better understanding of elements' naming convention
 print('''\nElements of matrix1:''')
-element = ''
+element = ''                            # Later will be used to construct each row of the matrix
 for vr in range(0, r):                  # vr - variable row
     for vc in range(0, c):              # vc - variable column
         element += 'M' + str(vr + 1) + str(vc + 1) + ' '
     print('|', element, '\b|')
     element = ''
 
-# Ask User for matrix elements and create lists
+# Create empty Matrix1 and fill it with user input element by element
 Matrix1 = [[] for vr in range(0, r)]
 for vr in range(0, r):
     Matrix1[vr] = [0 for vc in range(0, c)]
@@ -28,8 +26,8 @@ for vr in range(0, r):
     for vc in range(0, c):
         Matrix1[vr][vc] = int(input('M' + str(vr + 1) + str(vc + 1) + ': '))
 
-# Matrix2:
-v = int(input("Matrix2: Number of columns?: "))
+# Ask the user for dimension of second matrix
+v = int(input("Matrix2: Number of columns?: "))     # Only for columns since M2 rows = M1 columns by definition
 print('''\nElements of matrix Matrix2:''')
 element = ''
 for vr in range(0, c):                  # vr in c because Matrix 2 must have the same number of rows as Matrix1 columns.
@@ -38,7 +36,7 @@ for vr in range(0, c):                  # vr in c because Matrix 2 must have the
     print('|', element, '\b|')
     element = ''
 
-# Ask User for matrix elements and create lists
+# Create Empty Matrix2 and fill it with user input element by element
 Matrix2 = [[] for vr in range(0, c)]
 for vr in range(0, c):
     Matrix2[vr] = [0 for vc in range(0, v)]
@@ -47,14 +45,13 @@ for vr in range(0, c):
     for vc in range(0, v):
         Matrix2[vr][vc] = int(input('M' + str(vr + 1) + str(vc + 1) + ': '))
 
-# Create Empty Table for Product of Matrix and Vector
+# Create Product Matrix for Result of multiplication of Matrix1 and Matrix2
 Product = [[] for vr in range(0, r)]
 for vr in range(0, r):
     Product[vr] = [0 for columns in range(v)]
-print(Product)
-# Calculate the Product of two tables
 
-Products = 0
+# Calculate the Product of two tables
+Products = 0            # dummy variable for calculating sum of product of each row and column
 for vv in range(0, v):
     for vr in range(0, r):
         for vc in range(0, c):
@@ -67,6 +64,6 @@ print('Matrix1: ', Matrix1)
 print('Matrix2: ', Matrix2)
 print('Result of Matrix Multiplication:', Product)
 
-print('\n\n\ncreated by: MK')
+print('\n\ncreated by: MK')
 
 input('Press "Enter" to Exit')
