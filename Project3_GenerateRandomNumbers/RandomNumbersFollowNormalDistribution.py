787a1da27bd9059746fa68df3ab97f5dd5bd8e99
panpluto <panpluto@gmail.com>
2018-02-08 13:56:34 +0100

    Corrected height of 2.2% to be taken from population and not from sample.

--- Project3_GenerateRandomNumbers/RandomNumbersFollowNormalDistribution.py
+++ Project3_GenerateRandomNumbers/RandomNumbersFollowNormalDistribution.py
@@ -8,9 +8,9 @@ Men's height has mean of 175.5 cm and standard deviation of 7.4 cm
 Women's height has mean of 161.8 cm and standard deviation of 6.9 cm.
 
 Exercise:
-1. Show both distributions
+1. Show both samples
 2. Identify the minimum height of 2.2% of tallest people in both groups.
-
+3. Show the histogram
 
 
 RESULT:
@@ -21,15 +21,15 @@ import matplotlib.pyplot as plt
 
 mu_m, sigma_m = 175.5, 7.4
 mu_f, sigma_f = 161.8, 6.9
-j = 1000 - 0.02 * 1000
 
 female = numpy.random.normal(mu_f, sigma_f, 1000)
 print('1000 observations for women: ', [str('{0:.5g}'.format(i)) + ' cm' for i in female])
-print('Minium height of 2.2% tallest women is: ', '{0:.5g}'.format(sorted(female)[980]), 'cm.')
+# Because top 2.2% of normal distribution falls into mean + 2sd
+print('Minimum height of 2.2% tallest women is: ', '{0:.5g}'.format(mu_f + 2 * sigma_f), 'cm.')
 
 male = numpy.random.normal(mu_m, sigma_m, 1000)
 print('1000 observations for men: ', [str('{0:.5g}'.format(i)) + ' cm' for i in male])
-print('Minium height of 2.2% tallest men is:', '{0:.5g}'.format(sorted(male)[980]), 'cm.')
+print('Minimum height of 2.2% tallest men is:', '{0:.5g}'.format(mu_m + 2 * sigma_m), 'cm.')
 
 plt.hist(male, bins='auto', label='Male')
 plt.hist(female, bins='auto', label='Female')
