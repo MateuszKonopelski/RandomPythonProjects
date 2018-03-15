05cfa468e9c60d36ad298022a0df550d3fc413f2
Matt <panpluto@gmail.com>
2018-03-15 16:52:15 +0100

    All done

--- Project7_CodingBat_Solutions/Medium_CodingBat_Solutions.py
+++ Project7_CodingBat_Solutions/Medium_CodingBat_Solutions.py
@@ -114,4 +114,142 @@ def make_chocolate(small, big, goal):
       return (goal // 5 - big) * 5 + goal % 5
   return -1
 
+#Given a string, return a string where for every char in the original, there are two chars.
+
+def double_char(str):
+  string = ''
+  for char in str:
+    string += char * 2
+  return string
+
+#Return the number of times that the string "hi" appears anywhere in the given string.
+
+def count_hi(str):
+  count = 0
+  for i in range(len(str) - 1):
+    if str[i:i+2] == 'hi':
+      count += 1
+  return count
+
+#Return True if the string "cat" and "dog" appear the same number of times in the given string.
+
+def cat_dog(str):
+  cat = 0
+  dog = 0
+  for i in range(len(str)-2):
+    if str[i:i+3] == 'cat': cat += 1
+    if str[i:i+3] == 'dog': dog += 1
+  return cat == dog
+
+#Return the number of times that the string "code" appears anywhere in the given string, 
+#except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+def count_code(str):
+  count = 0
+  for i in range(len(str)-3):
+    if str[i:i+2] == 'co' and str[i+3] == 'e':
+      count += 1
+  return count
+
+#Given two strings, return True if either of the strings appears at the very end of the other 
+#string, ignoring upper/lower case differences (in other words, the computation should not be 
+#"case sensitive"). Note: s.lower() returns the lowercase version of a string.
+
+def end_other(a, b):
+  lena = len(a)
+  lenb = len(b)
+  
+  return b.lower() in a[-lenb:].lower() or a.lower() in b[-lena:].lower()
+
+#Return True if the given string contains an appearance of "xyz" where the xyz is not directly 
+#preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+def xyz_there(str):
+  
+  for i in range(len(str)-2):
+    if str[i:i+3] == 'xyz' and not str[i-1] == '.':
+      return True
+  return False
+
+#Return the number of even ints in the given array. Note: the % "mod" operator computes the 
+#remainder, e.g. 5 % 2 is 1.
+
+def count_evens(nums):
+  count = 0
+  for element in nums:
+    if element % 2 == 0:
+      count += 1
+  return count
+
+#Given an array length 1 or more of ints, return the difference between the largest and 
+#smallest values in the array. Note: the built-in min(v1, v2) and max(v1, v2) functions 
+#return the smaller or larger of two values.
+
+def big_diff(nums):
+  max_number = max(nums)
+  min_number = min(nums)
+  return max_number - min_number
+
+#Return the "centered" average of an array of ints, which we'll say is the mean average of 
+#the values, except ignoring the largest and smallest values in the array. If there are multiple 
+#copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int 
+#division to produce the final average. You may assume that the array is length 3 or more.
+
+def centered_average(nums):
+  newlist = sorted(nums)[1:-1]
+  sum = 0
+  for i in newlist:
+    sum += i
+  return sum / len(newlist)
+
+#Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 
+#13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+def sum13(nums):
+  for i in nums:
+	  if i == 13:
+		  try:
+			  del nums[nums.index(i)+1]
+			  del nums[nums.index(i)]
+		  except:
+			  del nums[nums.index(i)]
+  if len(nums) == 0 or nums == [13]:
+    return 0
+  else:
+    return sum(nums)
+  
+#Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and 
+#extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+def sum67(nums):
+    x = 0
+    while x < len(nums):
+        if nums[x] == 6:
+            y = x + 1
+            while y < len(nums):
+                if nums[y] == 7:
+                    del nums[x : y+1]
+                    x = 0
+                    break
+                y += 1
+        x += 1
+    return sum(nums)
+
+#Given an array of ints, return True if the array contains a 2 next to a 2 somewhere.
+
+def has22(nums):
+  if len(nums) == 1:
+    return False
+  for i in range(len(nums)+1):
+    try:
+      if nums[i] == 2:
+        if i>0:
+          if nums[i-1] == 2 or nums[i+1] == 2:
+            return True
+    except:
+      pass
+  return False
+
+
+  
+
 
